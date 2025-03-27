ee2caa8435b29b334897365f5f7030f645ca03d3
GiovZa <92822460+GiovZa@users.noreply.github.com>
2025-03-26 22:26:58 -0500

    Add AudioManager
    Create an Audio Manager that handles how the sound trigger script is utilized in the scene.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void Play(AudioClip clip, Vector3 position, float volume = 1f, bool spatial = true, AudioMixerGroup mixerGroup = null)
+    {
+        if (clip == null) return;
+
+        GameObject tempGO = new GameObject("TempAudio");
+        tempGO.transform.position = position;
+
+        AudioSource source = tempGO.AddComponent<AudioSource>();
+        source.clip = clip;
+        source.volume = volume;
+        source.spatialBlend = spatial ? 1f : 0f;
+        source.outputAudioMixerGroup = mixerGroup;
+        source.Play();
+
+        Destroy(tempGO, clip.length);
+    }
+}
