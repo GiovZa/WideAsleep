58a24add4ecab733cfdc1cc9db7ddba6587d6cd6
xiaoduanlongyi <zzlyiphone@icloud.com>
2025-04-29 13:14:26 -0500

    Door Open and Close

---
+++ Assets/Animation/Door/TriggerDoor.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TriggerDoor : MonoBehaviour
+{
+    Animator doorAnimator;
+    private bool inTriggerArea;
+
+    void Start()
+    {
+        doorAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("e") && inTriggerArea)
+        {
+            if(doorAnimator.GetBool("IsClosed"))
+                {
+                    doorAnimator.SetTrigger("Open");
+                    doorAnimator.SetBool("IsClosed", false);
+                }
+                else
+                {
+                    doorAnimator.SetTrigger("Close");
+                    doorAnimator.SetBool("IsClosed", true);
+                }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+            {
+                inTriggerArea = true;
+            }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+            {
+                inTriggerArea = false;
+            }
+    }
+}
+
